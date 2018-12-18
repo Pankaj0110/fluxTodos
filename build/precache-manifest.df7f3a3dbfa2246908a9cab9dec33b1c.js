23d9225634b1c10142567bb8ae3c6771522ef0c3
pankaj0110 <kumar.pankaj0110@gmail.com>
2018-12-19 00:56:19 +0530

    .ignore file build forlder is included

---
+++ build/precache-manifest.df7f3a3dbfa2246908a9cab9dec33b1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "822fdb7a7ac344ca9e9a",
+    "url": "/static/js/main.822fdb7a.chunk.js"
+  },
+  {
+    "revision": "24183a333e530e12e4fa",
+    "url": "/static/js/1.24183a33.chunk.js"
+  },
+  {
+    "revision": "822fdb7a7ac344ca9e9a",
+    "url": "/static/css/main.4a717e61.chunk.css"
+  },
+  {
+    "revision": "c9fbd049501da821d50b4e495df60e3e",
+    "url": "/index.html"
+  }
+];
